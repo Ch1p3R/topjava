eb0c3ca6cdec0106d6363c7adf4f89456ccc0687
Ch1p3R <7on@ukr.net>
2016-08-02 23:14:39 +0300

    my hw7

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -1,22 +1,74 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.to.UserMealWithExceed;
 import ru.javawebinar.topjava.util.TimeUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Objects;
+
+import static org.springframework.http.ResponseEntity.created;
 
 /**
  * GKislin
  * 06.03.2015.
  */
-@Controller
+@RestController
+@RequestMapping(value = UserMealRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserMealRestController extends AbstractUserMealController {
+    public static final String REST_URL = "/rest/profile/meals";
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @Override
+    public UserMeal get(@PathVariable("id") int id) {
+        return super.get(id);
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @Override
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    @Override
+    public List<UserMealWithExceed> getAll() {
+        return super.getAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Override
+    public void update(@RequestBody UserMeal meal,@PathVariable("id") int id) {
+        super.update(meal, id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserMeal> createWithLocation(@RequestBody UserMeal meal) {
+        UserMeal created = super.create(meal);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uri);
+        return new ResponseEntity<UserMeal>(created, httpHeaders, HttpStatus.CREATED);
+    }
+
+    @Override
+    public List<UserMealWithExceed> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+
+        return super.getBetween(startDate, startTime, endDate, endTime);
+
+    }
 }
